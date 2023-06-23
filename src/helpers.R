5f34f2faa2bf855aed305b9a7d2c95ddd4a5abcf
4g3nt81lly <4g3nt81lly@gmail.com>
2023-06-23 16:39:40 -0700

    Updated helpers.R
    1. Added docstrings for all functions in helpers.R.
    2. Removed `pixel.binning` as it's deprecated and no longer used in the project.

--- src/helpers.R
+++ src/helpers.R
@@ -1,14 +1,19 @@
+# AUTHOR: Bingyi (Billy) Li
+# CREATED: 2023-06-21
+
+
 # ======= Hilbert Curve =======
 
+
+#' log_2(N)-order Hilbert Curve Coordinate at Index
+#'
+#' Generate a coordinate of log_2(N)'th-order Hilbert Curve at a given index.
+#'  Note: This iterative implementation is inspired by (this article)[https://blog.marcinchwedczuk.pl/iterative-algorithm-for-drawing-hilbert-curve.]
+#' @param index  The index, within the domain [0, N^2]
+#' @param N      The side length of the square formed by the Hilbert Curve.
+#'
+#' @return       The coordinate in a 2D euclidean plane in the form (x, y).
 hilbert.coordinate <- function(index, N) {
-    #' log_2(N)-order Hilbert Curve Coordinate at Index
-    #'
-    #' Generate a coordinate of log_2(N)'th-order Hilbert Curve at a given index.
-    #' @param index  The index, within the domain [0, N^2]
-    #' @param N      The side length of the square formed by the Hilbert Curve.
-    #'
-    #' @return       The coordinate in a 2D euclidean plane in the form (x, y).
-    
     # base case patterns
     positions <- list(list(0, 0), # case 1
                       list(0, 1), # case 2
@@ -53,16 +58,15 @@ hilbert.coordinate <- function(index, N) {
     return(c(x, y))
 }
 
+#' log_2(N)-order Hilbert Curve Map
+#'
+#' Generate a mapping data frame (coordinates) of log_2(N)-order Hilbert Curve
+#'  used for decoding images stored in that sequence.
+#' Note: The coordinates are in R's 1-based indexing
+#' @param N  The side length of the square formed by the Hilbert Curve.
+#'
+#' @return   A N^2 x 2 data frame with row-wise 2D Hilbert-Curve coordinates.
 get.hilbert.map <- function(N) {
-    #' log_2(N)-order Hilbert Curve Map
-    #'
-    #' Generate a mapping data frame (coordinates) of log_2(N)-order Hilbert Curve used
-    #'   for decoding images stored in the sequence of the N-order Hilbert curve.
-    #' Note: The coordinates are in R's 1-based indexing
-    #' @param N  The side length of the square formed by the Hilbert Curve.
-    #'
-    #' @return   A N^2 x 2 data frame with row-wise 2D Hilbert-Curve coordinates.
-    
     hilbert.map <- tibble(i = seq_len(N ^ 2) - 1) |>
         rowwise() |>
         mutate(coord = paste(hilbert.coordinate(i, N) + 1, collapse = ' '),
@@ -71,6 +75,14 @@ get.hilbert.map <- function(N) {
     return(hilbert.map)
 }
 
+#' Coordinate Pairs
+#'
+#' Generate coordinate pairs for rendering relevant graphics (e.g. segments,
+#'  arrows, etc).
+#' Note: For N coordinates, there will be N - 1 coordinate pairs.
+#' @param coords  A data frame for coordinates with columns `x` and `y`.
+#'
+#' @return        An (N - 1)-row data frame with row-wise coordinate pairs.
 get.coordinate.pairs <- function(coords) {
     coords |>
         lag(1) |>
@@ -79,16 +91,16 @@ get.coordinate.pairs <- function(coords) {
         slice(-1)
 }
 
+#' Plot a log_2(N)-order Hilbert Curve
+#'
+#' Plot a log_2(N)-order Hilbert Curve with a given mapping data frame.
+#' @param hilbert.map  A data frame returned by the helper function `get.hilbert.map`.
+#'
+#' @return             A plot with a log_2(N)-order Hilbert Curve.
 hilbert.curve.demo <- function(hilbert.map) {
-    #' Plot a log_2(N)-order Hilbert Curve
-    #'
-    #' Plot a log_2(N)-order Hilbert Curve with a given mapping data frame.
-    #' @param hilbert.map  A data frame returned by the helper function `get.hilbert.map`.
-    #'
-    #' @return             A plot.
-    
     N <- sqrt(nrow(hilbert.map))
     
+    # convert to cartesian coordinates
     hilbert.map <- hilbert.map |>
         map_at('y', ~ N - . + 1) |>
         as_tibble()
@@ -96,14 +108,16 @@ hilbert.curve.demo <- function(hilbert.map) {
     hilbert.demo.plot <- hilbert.map |>
         get.coordinate.pairs() |>
         ggplot(aes(x = x0, y = y0, xend = x1, yend = y1)) +
+            # arrows: demonstrating the sequence
             geom_segment(arrow = arrow(length = unit(0.25, 'cm'), type = 'closed'),
                          color = 'darkgray') +
+            # each point is mapped to a single feature
             geom_point(data = hilbert.map, aes(x = x, y = y),
                        inherit.aes = FALSE) +
             theme_bw() +
+            # no grids and axes
             theme(panel.grid.major = element_blank(),
-                  panel.grid.minor = element_blank(),  
-                  # no axes
+                  panel.grid.minor = element_blank(),
                   axis.ticks = element_blank(),
                   axis.text = element_blank(),
                   axis.title = element_blank())
@@ -112,8 +126,16 @@ hilbert.curve.demo <- function(hilbert.map) {
 }
 
 
-# ======= Datasets =======
+# ======= Data Sets =======
 
+
+#' Data Set Summary
+#'
+#' Return an overall class summary for a train/test split.
+#' @param training.set  A training set.
+#' @param testing.set   A testing set.
+#'
+#' @return              A summary data frame.
 get.summary <- function(training.set, testing.set) {
     training.summary <- training.set |>
         group_by(label) |>
@@ -129,6 +151,16 @@ get.summary <- function(training.set, testing.set) {
     return(overall.summary)
 }
 
+#' Data Set Transformation
+#'
+#' Transform all images of a data set by a given function.
+#'  Note: This is a more general helper function for data augmentation.
+#' @param data.set  A data set to be 
+#' @param fn        A transformation function that takes in a feature vector
+#'                   and returns a transformed feature vector.
+#' @param size      A promised image size after the transformation.
+#'
+#' @return          A transformed data set.
 transform.set <- function(data.set, fn, size = NULL, ...) {
     # get columns for image pixels ONLY
     image.set <- data.set |>
@@ -147,8 +179,8 @@ transform.set <- function(data.set, fn, size = NULL, ...) {
         new.set <- new.set |> rbind(transformed)
     }
     new.set <- as.data.frame(new.set)
+    # restore labels if exists
     if ('label' %in% colnames(data.set)) {
-        # restore labels
         new.set <- mutate(new.set, label = data.set$label)
     }
     # getting rid of auto-generated row names
@@ -156,47 +188,68 @@ transform.set <- function(data.set, fn, size = NULL, ...) {
     return(new.set)
 }
 
+#' Data Augmentation
+#'
+#' Generate an altered copy of the given data set with the given
+#'  transformation function.
+#' @param data.set  A data set to be augmented.
+#' @param fn        A transformation function that takes in a matrix and
+#'                   outputs a matrix.
+#' @param ...       Variadic arguments passed to `fn`.
+#'
+#' @return          An augmented data set.
 augment.set <- function(data.set, fn, ...) {
-    #' Data Augmentation
-    #'
-    #' Generate an altered copy of the dataset with the given transformation function.
-    #' @param data.set  A data frame to be altered.
-    #' @param fn        A transformation function that takes in a matrix and outputs a matrix.
-    #' @param ...       Variadic arguments passed to `fn`.
-    #'
-    #' @return          A data frame with transformed observations.
-    
-    # get columns for image pixels ONLY
-    
-    image.dim <- ncol(data.set |> select_if(is.numeric))
+    # get image size
+    image.size <- ncol(data.set |> select_if(is.numeric))
     
     transform.set(data.set, function(image.array, ...) {
         image.array |>
-            as.image.matrix(ncol = sqrt(image.dim)) |>
+            # convert to matrix
+            as.image.matrix(ncol = sqrt(image.size)) |>
             fn(...) |>
             as.feature.vector()
     }, ...)
 }
 
+
 # ======= Images =======
 
+
+#' Image Matrix
+#'
+#' A wrapper function that converts a feature vector to an N x M matrix.
+#'  Note: At least one of `nrow = ...` and `ncol = ...` should be provided.
+#' @param vec  A one-dimensional feature vector.
+#' @param ...  Variadic arguments passed to `matrix`, see note.
+#'
+#' @return          An N x M matrix.
 as.image.matrix <- function(vec, ...) {
     vec |> matrix(byrow = TRUE, ...)
 }
 
+#' Feature Vector
+#'
+#' A wrapper function that converts an N x M matrix to a feature vector.
+#'  Note: An inverse (almost) operation of `as.image.matrix`.
+#' @param mtx  An N x M matrix.
+#'
+#' @return          A one-dimensional numeric feature vector.
 as.feature.vector <- function(mtx) {
+    # t() for conversion by row
+    # R's default conversion does it by column
     mtx |> t() |> as.numeric()
 }
 
+#' Display Image from Data Set
+#'
+#' Display the i'th image from a given data set.
+#'  Note: This helper function is capable of performing inverse HC
+#'  linearization on demand.
+#' @param data.set  A data set.
+#' @param i         The index of the desired image.
+#' @param n         The side length hint of the image.
+#' @param hilbert   Whether the image is HC linearized.
 show.image <- function(data.set, i = 1, n = 16, hilbert = FALSE, ...) {
-    #' Display an image from a dataset
-    #'
-    #' Display the i'th image (matrix/array) from a given dataset.
-    #' @param data.set  A data frame.
-    #' @param i         The number (index) of the desired image.
-    #' @param n         The side length hint of the image.
-    #' @param hilbert   Whether or not the image is arranged by Hilbert Curve.
-    
     # get image vector from data frame at i'th row
     image.data <- data.set |>
         select_if(is.numeric) |>
@@ -236,18 +289,18 @@ show.image <- function(data.set, i = 1, n = 16, hilbert = FALSE, ...) {
     }
 }
 
+#' X-padding
+#'
+#' Padding a given matrix with a given number or random numbers within a given
+#'  range.
+#' @param mtx     A matrix to be X-padded.
+#' @param n       The number of layers to pad.
+#' @param x       A number, or a random number range for X-padding the matrix.
+#' @param random  Whether the matrix is X-padded with random numbers.
+#'                The parameter `x` will be interpreted as a range if `TRUE`.
+#'
+#' @return        An X-padded matrix.
 X.padding <- function(mtx, n = 1, x = 1, random = FALSE) {
-    #' X-padding
-    #'
-    #' Zero-padding a given matrix with a given number or random numbers within a given range.
-    #' @param mtx     A matrix to be X-padded.
-    #' @param n       The number of layers to pad.
-    #' @param x       A number, or a random number range for X-padding the matrix.
-    #' @param random  Whether or not the matrix is X-padded with random numbers.
-    #'                The parameter `x` will be interpreted as a range is `TRUE`.
-    #'
-    #' @return        A X-padded matrix.
-    
     if (random) {
         new.nrow <- nrow(mtx) + 2 * n
         # generate random paddings within a given range
@@ -273,40 +326,15 @@ X.padding <- function(mtx, n = 1, x = 1, random = FALSE) {
     return(mtx)
 }
 
-pixel.binning <- function(image.array, binsize = 2) {
-    #' One-dimensional X Pixel Binning
-    #'
-    #' Perform a 1 x N pixel binning on a given image array.
-    #' @param image.array  An image array of the dimension 1 x N.
-    #' @param binsize      The size of the bin by which the image array is compressed.
-    #'                     The default is `2L`.
-    #'
-    #' @return        A pixel-binned image array (vector).
-    
-    scaled.array <- image.array |>
-        as.image.matrix(ncol = binsize) |>
-        as.data.frame() |>
-        rowwise() |> mutate(avg = mean(c_across(everything()))) |>
-        pull(avg)
-    return(scaled.array)
-}
-
-compress.image <- function(image.array) {
-    ((lag(image.array, 1) + image.array) / 2) |>
-        na.omit() |>
-        as.numeric()
-}
-
+#' Matrix Rotation
+#'
+#' Perform a clockwise rotation on a given matrix by a given angle.
+#' @param mtx    A matrix.
+#' @param angle  The rotation angle in degrees.
+#'               Note: Must be an integer multiple of 90 degrees.
+#'
+#' @return       A rotated matrix.
 rotate.image <- function(mtx, angle = 90) {
-    #' Matrix Rotation
-    #'
-    #' Perform a clockwise rotation on a given matrix by a given angle.
-    #' @param mtx    A matrix.
-    #' @param angle  The rotation angle in degrees.
-    #'               Note: Must be an integer multiple of 90 degrees.
-    #'
-    #' @return       A rotated matrix.
-    
     if (angle %% 90 == 0) {
         angle <- angle %% 360
         while (angle > 0) {
@@ -318,8 +346,19 @@ rotate.image <- function(mtx, angle = 90) {
     stop('rotation angle must be an integer multiple of 90 degrees')
 }
 
+#' Additive Gaussian Noise
+#'
+#' Add Gaussian Noise to a given matrix.
+#'  Note: By default, a standard Gaussian distribution is used.
+#' @param mtx   A matrix.
+#' @param mean  The mean of the Gaussian distribution.
+#' @param sd    The standard deviation of the Gaussian distribution.
+#'
+#' @return       A matrix with noise added.
 gaussian.noise <- function(mtx, mean = 0, sd = 1) {
-    gaussian.filter <- rnorm(nrow(mtx) * ncol(mtx), mean = mean, sd = sd) |>
+    # generate a Gaussian filter
+    gaussian.filter <- rnorm(nrow(mtx) * ncol(mtx),
+                             mean = mean, sd = sd) |>
         matrix(nrow = nrow(mtx))
     return(mtx + gaussian.filter)
 }
